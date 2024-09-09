384cb9df51ddd8f8ef74b5ed43cc9a80826ce8aa
rajat <agrawal21rajat@gmail.com>
2024-09-09 21:51:47 +0530

    File changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const signb = document.querySelector(".stn")
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,3 +10,13 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+signb.addEventListener("click", ()=>{
+    event.preventDefault();
+    console.log("sign in button clicked");
+    window.location.href = "/Hospital/patientdash.html"
+     
+})
+
+
